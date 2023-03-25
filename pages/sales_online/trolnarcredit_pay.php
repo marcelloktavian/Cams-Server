8cd2cdb531d31cc94bd3429ad4a09c3c60ca2a6c
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-03-25 12:22:18 +0700

    Update trolnarcredit_pay.php

--- pages/sales_online/trolnarcredit_pay.php
+++ pages/sales_online/trolnarcredit_pay.php
@@ -23,7 +23,7 @@
       <label for="sisa_piutang" class="ui-helper-reset label-control">Sisa Piutang</label>
       <div class="ui-corner-all form-control">
         <input value="<?= number_format($_GET['sisa_piutang'], 0) ?>" type="text" class="required" id="" name="" style="background-color:#eae8dd;" readonly>
-        <input value="<?= $_GET['sisa_piutang'] ?>" type="hidden" class="required" id="sisa_piutang" name="sisa_piutang" style="background-color:#eae8dd;" readonly>
+        <input value="<?= round($_GET['sisa_piutang']) ?>" type="hidden" class="required" id="sisa_piutang" name="sisa_piutang" style="background-color:#eae8dd;" readonly>
       </div>
       <input id="no_akun" name="no_akun" value="<?= $_GET['no_akun'] ?>" hidden>
     </form>
