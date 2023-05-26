6eb0a61b06a3f9579e21e9e53fb6c01ef0e77452
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-05-26 15:31:10 +0700

    update pembetulan pembulatan invoice

--- pages/transaksi_purchase/po_invoice_add.php
+++ pages/transaksi_purchase/po_invoice_add.php
@@ -117,9 +117,9 @@
     for(var i=1; i<=baris1; i++){
       var kode = $('#id'+i).val();
       if(kode != undefined && kode != ''){
-        var subtotal_item = Math.round(parseFloat($('#qty_inv'+i).val())*parseFloat($('#dpp_unit'+i).val()));
+        var subtotal_item = parseFloat($('#qty_inv'+i).val())*parseFloat($('#dpp_unit'+i).val());
         var ppn_item = $('#persen_ppn'+i).val();
-        $('#subtotal_inv'+i).val(parseFloat(subtotal_item)+Math.floor((parseInt(subtotal_item*ppn_item/100))));
+        $('#subtotal_inv'+i).val(Math.round(parseFloat(subtotal_item)+Math.floor((parseInt(subtotal_item*ppn_item/100)))));
       }
     }
   }
