159caa998ac0e69121b242dbc80fd601a9c5801c
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-05-26 15:44:30 +0700

    test pembulatan

--- pages/transaksi_purchase/po_invoice_add.php
+++ pages/transaksi_purchase/po_invoice_add.php
@@ -119,7 +119,9 @@
       if(kode != undefined && kode != ''){
         var subtotal_item = Math.ceil(parseFloat($('#qty_inv'+i).val())*parseFloat($('#dpp_unit'+i).val()));
         var ppn_item = $('#persen_ppn'+i).val();
-        $('#subtotal_inv'+i).val(Math.round(parseFloat(subtotal_item)+Math.floor((parseInt(subtotal_item*ppn_item/100)))));
+        console.log(subtotal_item);
+        console.log(subtotal_item*ppn_item/100);
+        $('#subtotal_inv'+i).val(parseFloat(subtotal_item)+Math.floor((parseInt(subtotal_item*ppn_item/100))));
       }
     }
   }
