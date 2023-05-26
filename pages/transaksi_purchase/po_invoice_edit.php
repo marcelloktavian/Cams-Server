9573127a0302c6de8373723a783a724fe7b21734
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-05-26 16:07:36 +0700

    update ppn

--- pages/transaksi_purchase/po_invoice_edit.php
+++ pages/transaksi_purchase/po_invoice_edit.php
@@ -120,7 +120,7 @@
       if(kode != undefined && kode != ''){
         var subtotal_item = Math.round(parseFloat($('#qty_inv'+i).val())*parseFloat($('#dpp_unit'+i).val()));
         var ppn_item = $('#persen_ppn'+i).val();
-        $('#subtotal_inv'+i).val(parseFloat(subtotal_item)+Math.floor((parseInt(subtotal_item*ppn_item/100))));
+        $('#subtotal_inv'+i).val(Math.round(parseFloat(subtotal_item))+Math.floor((parseInt(subtotal_item*ppn_item/100))));
       }
     }
   }
