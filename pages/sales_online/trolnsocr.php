0d9e70a5bdea329e358108c95a1c20c0475462e4
황진홍 <william1412.surya@gmail.com>
2023-06-03 12:36:44 +0700

    update pembetulan dpp dan ppn trolnsocr ke jurnal

--- pages/sales_online/trolnsocr.php
+++ pages/sales_online/trolnsocr.php
@@ -171,9 +171,14 @@ $allow_delete = is_show_menu(DELETE_POLICY, OnlineCredit, $group_acess);
         //get master id terakhir
         $q = mysql_fetch_array( mysql_query('select id FROM jurnal order by id DESC LIMIT 1'));
         $idparent=$q['id'];
-		
-		$dpp = round($total / 1.11);
-		$ppn = round($total / 1.11 * 0.11);
+
+		$if($exp_fee == 0){
+			$dpp = round($total / 1.11);
+			$ppn = round($total / 1.11 * 0.11);
+		}else{
+			$dpp = round(($total-$exp_fee) / 1.11);
+			$ppn = round(($total-$exp_fee) / 1.11 * 0.11);
+		}
 
 		$query1=mysql_query("SELECT id, noakun, nama, 'Detail' AS `status` FROM det_coa WHERE noakun=CONCAT('01.04.',IF(LENGTH('$dropshipper')=1,'0000',IF(LENGTH('$dropshipper')=2,'000',IF(LENGTH('$dropshipper')=3,'00',IF(LENGTH('$dropshipper')=4,'0','')))), '$dropshipper')");
 		while($akun1 = mysql_fetch_array($query1)){
