1dad0c85e39fc31a4cf0af747b359492e78ec76d
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-07-21 14:35:19 +0700

    Update arb2b_get_akun.php

--- pages/sales_b2b/arb2b_get_akun.php
+++ pages/sales_b2b/arb2b_get_akun.php
@@ -4,7 +4,7 @@ include "../../include/koneksi.php";
 if(isset($_GET['action']) && $_GET['action'] == 'akunkredit'){
   $id = $_GET['id'];
 
-  $sql_cmd = "SELECT c.id AS id_akun, c.`noakun` AS nomor_akun, c.nama AS nama_akun FROM mst_b2bcustomer b LEFT JOIN det_coa c ON c.noakun = CONCAT('04.03.', LPAD(b.id, 5, 0)) WHERE b.id='".$id."'";
+  $sql_cmd = "SELECT c.id AS id_akun, c.`noakun` AS nomor_akun, c.nama AS nama_akun FROM mst_b2bcustomer b LEFT JOIN det_coa c ON c.noakun = CONCAT('01.05.', LPAD(b.id, 5, 0)) WHERE b.id='".$id."'";
 
   $sql = mysql_query($sql_cmd);
   $sql = mysql_fetch_array($sql);
