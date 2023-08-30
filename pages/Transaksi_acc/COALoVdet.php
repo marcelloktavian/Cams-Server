df10fb38f913b3e814cb4a0a3c7e284f6736d3cc
황진홍 <william1412.surya@gmail.com>
2023-08-30 15:12:45 +0700

    Update COALoVdet.php

--- pages/Transaksi_acc/COALoVdet.php
+++ pages/Transaksi_acc/COALoVdet.php
@@ -7,7 +7,7 @@ $sql = mysql_query($sql_cmd);
 $num = mysql_num_rows($sql);
 
 if($num == 0){
-    $sql_cmd ="SELECT *, 'detail' as `status` from det_coa WHERE noakun = '$id' LIMIT 1";
+    $sql_cmd ="SELECT det_coa.*, 'detail' as `status` from det_coa LEFT JOIN mst_coa ON mst_coa.id=det_coa.id_parent WHERE mst_coa.deleted=0 AND mst_coa.noakun = '$id' LIMIT 1";
     $sql = mysql_query($sql_cmd);
     $row = mysql_fetch_array($sql);
     echo json_encode($row); 
