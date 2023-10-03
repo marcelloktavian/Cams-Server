f04513c29c7f89113d241b83dad04f1075ed3393
황진홍 <william1412.surya@gmail.com>
2023-10-03 13:17:58 +0700

    update bug checker produk keluar

---
+++ pages/checker/produk-keluar-check.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <form action="produk-keluar-process.php" method="post" enctype="multipart/form-data">
+      <input type="file" id='excel' name='excel_file' />
+      <input type="date" id='date' name='tanggalolnso' />
+      <input type="submit">
+    </form>
+  </body>
+</html>
---
+++ pages/checker/produk-keluar-process.php
@@ -0,0 +1,68 @@
+<?php
+
+include '../../include/koneksi.php';
+require 'PHPExcel.php';
+
+ini_set('max_execution_time', 3000);
+
+?>
+
+
+<style>
+    tr:nth-child(even){
+      background: lightgray;
+    }
+
+    td:nth-child(even){
+      font-weight: bold;
+    }
+  </style>
+
+<center>
+  <h1>File Compare Result</h1>
+  <h3><?= $_FILES['excel_file']['name'] ?></h3>
+
+  <?php
+
+  $date = $_POST['tanggalolnso'];
+
+  $excel_file = $_FILES['excel_file']['tmp_name'];
+  $excel = PHPExcel_IOFactory::load($excel_file);
+
+  $data = [];
+
+  foreach ($excel->getWorksheetIterator() as $worksheet) {
+    foreach ($worksheet->getRowIterator() as $row) {
+      $details = $worksheet->getCellByColumnAndRow(1, $row->getRowIndex())->getValue();
+
+      if($details != '' OR $details != null){
+        $data[] = [
+          'nama' => $worksheet->getCellByColumnAndRow(1, $row->getRowIndex())->getValue(),
+          'total' => $worksheet->getCellByColumnAndRow(23, $row->getRowIndex())->getValue(),
+        ];
+      }
+    }
+  }
+  ?>
+
+  <table border = 1 width=100%>
+    <tr><th>No</th><th>Barang</th><th>Qtyd Excel</th><th>Qty Database</th></tr>
+
+  <?php 
+  $i = 1;
+  
+  foreach($data as $line){
+    $query = mysql_query("SELECT COALESCE(SUM('a.namabrg'),0) AS total_barang FROM olnsodetail a LEFT JOIN olnso b ON a.id_trans=b.id_trans WHERE a.namabrg = '".$line['nama']."' AND b.lastmodified = '".$_POST['tanggalolnso']."' GROUP BY a.namabrg");
+    $olnso = mysql_fetch_array($query);
+    ?>
+    <tr <?= ($line['total'] != $olnso['total_barang']) ? "style='background-color:lightred';" : "" ?>>
+      <td><?= $i ?></td>
+      <td><?= $line['nama'] ?></td>
+      <td><?= $line['total'] ?></td>
+      <td><?= $olnso['total_barang'] ?></td>
+    </tr>
+    <p> </p>
+    <?php
+    $i ++;
+  }?>
+  </table>
