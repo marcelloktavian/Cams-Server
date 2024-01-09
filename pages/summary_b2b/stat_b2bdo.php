1f7028aa3fe9c26d26bf5d15ea48fe9b3e2984b0
= <=>
2024-01-09 10:27:06 +0700

    update perubahan nama module 09/01/24

--- pages/summary_b2b/stat_b2bdo.php
+++ pages/summary_b2b/stat_b2bdo.php
@@ -1,57 +1,57 @@
 <?php require_once '../../include/config.php' ?>
 <?php
-$group_acess = unserialize(file_get_contents("../../GROUP_ACCESS_CACHE".$_SESSION['user']['group_id']));
+$group_acess = unserialize(file_get_contents("../../GROUP_ACCESS_CACHE" . $_SESSION['user']['group_id']));
 ?>
 <div class="ui-widget ui-form" style="margin-bottom:5px">
- <div class="ui-widget-header ui-corner-top padding5">
-		B2B Sales Statistic Report
-    </div>
-    <div class="ui-widget-content ui-corner-bottom">
-        <form id="report_project_form" method="" action="" class="ui-helper-clearfix">
-        	<label for="project_id" class="ui-helper-reset label-control">Tanggal</label>
-            <div class="ui-corner-all form-control">
-            	<table>
-				<tr>
-				<td>
-				 <input value="" type="text" class="required datepicker"   id="startdate_statb2bdo" name="startdate_statb2bdo">
-				</td>
-				<td> s.d.  
-				<input value="" type="text" class="required datepicker"  id="enddate_statb2bdo" name="enddate_statb2bdo">
-				</td>
-				<!-- <td> Filter -->
-				 <!-- <input value="" type="hidden" id="filteroperasional" name="filteroperasional"> -->
-				 <!-- (Nama Biaya,Tanggal,Subtotal,PPN,Total) -->
-				<!-- </td> -->
-				</tr>
+	<div class="ui-widget-header ui-corner-top padding5">
+		B2B Laporan Penjulalan
+	</div>
+	<div class="ui-widget-content ui-corner-bottom">
+		<form id="report_project_form" method="" action="" class="ui-helper-clearfix">
+			<label for="project_id" class="ui-helper-reset label-control">Tanggal</label>
+			<div class="ui-corner-all form-control">
+				<table>
+					<tr>
+						<td>
+							<input value="" type="text" class="required datepicker" id="startdate_statb2bdo" name="startdate_statb2bdo">
+						</td>
+						<td> s.d.
+							<input value="" type="text" class="required datepicker" id="enddate_statb2bdo" name="enddate_statb2bdo">
+						</td>
+						<!-- <td> Filter -->
+						<!-- <input value="" type="hidden" id="filteroperasional" name="filteroperasional"> -->
+						<!-- (Nama Biaya,Tanggal,Subtotal,PPN,Total) -->
+						<!-- </td> -->
+					</tr>
 				</table>
-            </div>
-			<label for="project_id" class="ui-helper-reset label-control">Type</label>
-            <div class="ui-corner-all form-control">
-            	<table>
-				<tr>
-				<td>
-				 <select name='type_statb2bdo' id='type_statb2bdo'>
-					 <option value='1'>Normal (Alphabetical)</option>
-					 <option value='2'>Best Seller</option>
-				 </select>
-				</td>
-				</tr>
-				<tr>
-					<td></td>
-				</tr>
-				</table>
-            <label for="" class="ui-helper-reset label-control">&nbsp;</label>
-            <div class="ui-corner-all form-control">
-				<button onclick="printrptstatb2bdo()" class="btn" type="button">Cetak</button>
-				<!-- <button onclick="printrptharian()" class="btn" type="button">Cetak Harian</button> -->
 			</div>
-       	</form>
-   	</div>
+			<label for="project_id" class="ui-helper-reset label-control">Type</label>
+			<div class="ui-corner-all form-control">
+				<table>
+					<tr>
+						<td>
+							<select name='type_statb2bdo' id='type_statb2bdo'>
+								<option value='1'>Normal (Alphabetical)</option>
+								<option value='2'>Best Seller</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+					</tr>
+				</table>
+				<label for="" class="ui-helper-reset label-control">&nbsp;</label>
+				<div class="ui-corner-all form-control">
+					<button onclick="printrptstatb2bdo()" class="btn" type="button">Cetak</button>
+					<!-- <button onclick="printrptharian()" class="btn" type="button">Cetak Harian</button> -->
+				</div>
+		</form>
+	</div>
 </div>
- 
+
 <!--
 <?php
-	/*
+/*
 	$allow = array(1,2,3);
 	if(in_array($_SESSION['user']['access'], $allow)) {
 		echo '<button type="button" onclick="javascript:popup_form(\''.BASE_URL.'pages/beli.php?action=add\',\'table_beli\')" class="btn">Tambah</button>';		
@@ -61,22 +61,21 @@ $group_acess = unserialize(file_get_contents("../../GROUP_ACCESS_CACHE".$_SESSIO
 -->
 
 <script type="text/javascript">
-	 
 	$('#startdate_statb2bdo').datepicker({
 		dateFormat: "dd/mm/yy"
 	});
 	$('#enddate_statb2bdo').datepicker({
 		dateFormat: "dd/mm/yy"
 	});
-	$( "#startdate_statb2bdo" ).datepicker( 'setDate', '<?php echo date('d/m/Y')?>' );
-	$( "#enddate_statb2bdo" ).datepicker( 'setDate', '<?php echo date('d/m/Y')?>' );
-	
+	$("#startdate_statb2bdo").datepicker('setDate', '<?php echo date('d/m/Y') ?>');
+	$("#enddate_statb2bdo").datepicker('setDate', '<?php echo date('d/m/Y') ?>');
+
 	function printrptstatb2bdo() {
 		var startdate = $('#startdate_statb2bdo').val();
 		var enddate = $('#enddate_statb2bdo').val();
 		// console.log(filter+' '+lokasi_list);
 
-		window_open('<?php echo BASE_URL ?>pages/summary_b2b/stat_b2bdo_rpt.php?action=preview&type='+$('#type_statb2bdo').val()+'&start='+startdate+'&end='+enddate);
-		
+		window_open('<?php echo BASE_URL ?>pages/summary_b2b/stat_b2bdo_rpt.php?action=preview&type=' + $('#type_statb2bdo').val() + '&start=' + startdate + '&end=' + enddate);
+
 	}
 </script>
--- pages/superadmin_menu/lap_periodeolnb2b.php
+++ pages/superadmin_menu/lap_periodeolnb2b.php
@@ -2,89 +2,89 @@
 
 <div class="ui-widget ui-form">
     <div class="ui-widget-header ui-corner-top padding5">
-        Laporan Periode OLN+B2B
+        OLN + B2B Laporan Penjualan
     </div>
     <div class="ui-widget-content ui-corner-bottom">
         <form id="dpsum_form" method="" action="" class="ui-helper-clearfix">
             <label for="project_id" class="ui-helper-reset label-control">Date</label>
             <div class="ui-corner-all form-control">
                 <table>
-                <tr>
-                <td>
-                 <input value="<?php echo date('d/m/Y')?>" type="text" class="required datepicker" id="lapolnb2bstartdate" name="lapolnb2bstartdate">
-                </td>
-                <td>
-                 s/d.
-                </td>
-                <td>
-                 <input value="<?php echo date('d/m/Y')?>" type="text" class="required datepicker" id="lapolnb2benddate" name="lapolnb2benddate">
-                </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <input value="<?php echo date('d/m/Y') ?>" type="text" class="required datepicker" id="lapolnb2bstartdate" name="lapolnb2bstartdate">
+                        </td>
+                        <td>
+                            s/d.
+                        </td>
+                        <td>
+                            <input value="<?php echo date('d/m/Y') ?>" type="text" class="required datepicker" id="lapolnb2benddate" name="lapolnb2benddate">
+                        </td>
+                    </tr>
                 </table>
-                
+
             </div>
             <label for="project_id" class="ui-helper-reset label-control">Type</label>
             <div class="ui-corner-all form-control">
                 <table>
-                <tr>
-                <td>
-                 <select name='type' id='type'>
-                     <option value='1'>Normal (Alphabetical)</option>
-                     <option value='2'>Best Seller</option>
-                 </select>
-                </td>
-                </tr>
-                <tr>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td>
+                            <select name='type' id='type'>
+                                <option value='1'>Normal (Alphabetical)</option>
+                                <option value='2'>Best Seller</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
                 </table>
-            <!--
+                <!--
             <label for="lblcust_jual" class="ui-helper-reset label-control">Expedition</label>
             
             <div class="ui-corner-all form-control">
                 <select class="required" name="exp_list" id="exp_list">
                     <option value="">-pilih-</option>
                     <?php
-                        
-                        $query = $db->query("SELECT * FROM mst_expedition where deleted=0 ORDER BY nama ASC");
-                        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
-                        foreach($rows as $r) {
-                            $select = isset($row['id']) && $row['id'] == $r['id'] ? 'selected' : ''; 
-                            echo '<option '.$select.' value="'.$r['id'].'">'.$r['nama'].'</option>';
-                        }
-                        
+
+                    $query = $db->query("SELECT * FROM mst_expedition where deleted=0 ORDER BY nama ASC");
+                    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($rows as $r) {
+                        $select = isset($row['id']) && $row['id'] == $r['id'] ? 'selected' : '';
+                        echo '<option ' . $select . ' value="' . $r['id'] . '">' . $r['nama'] . '</option>';
+                    }
+
                     ?>
                 </select>
             </div>
             -->
-            <label for="" class="ui-helper-reset label-control">&nbsp;</label>
-            <div class="ui-corner-all form-control">
-                                 <?php
-                $statusToko = '';
-    $getStat = $db->prepare("SELECT * FROM tbl_status LIMIT 1");
-    $getStat->execute();
-    $stat = $getStat->fetchAll();
-    foreach ($stat as $stats) {
-        $statusToko = $stats['status'];
-    }
-    
-    if ($statusToko == 'Tutup') {
-        echo '<button type="button" onclick="javascript:custom_alert(\'Maaf, Toko Sudah Tutup\')" class="btn">Print</button>';
-    }else{
-                ?>
-                <button onclick="javascript:window_open('<?php echo BASE_URL ?>pages/superadmin_menu/lap_periodeolnb2b_print.php?action=preview&type='+$('#type').val()+'&start='+$('#lapolnb2bstartdate').val()+'&end='+ $('#lapolnb2benddate').val() )" class="btn" type="button">Print</button>
-                 <?php } ?>
-            </div>
+                <label for="" class="ui-helper-reset label-control">&nbsp;</label>
+                <div class="ui-corner-all form-control">
+                    <?php
+                    $statusToko = '';
+                    $getStat = $db->prepare("SELECT * FROM tbl_status LIMIT 1");
+                    $getStat->execute();
+                    $stat = $getStat->fetchAll();
+                    foreach ($stat as $stats) {
+                        $statusToko = $stats['status'];
+                    }
+
+                    if ($statusToko == 'Tutup') {
+                        echo '<button type="button" onclick="javascript:custom_alert(\'Maaf, Toko Sudah Tutup\')" class="btn">Print</button>';
+                    } else {
+                    ?>
+                        <button onclick="javascript:window_open('<?php echo BASE_URL ?>pages/superadmin_menu/lap_periodeolnb2b_print.php?action=preview&type='+$('#type').val()+'&start='+$('#lapolnb2bstartdate').val()+'&end='+ $('#lapolnb2benddate').val() )" class="btn" type="button">Print</button>
+                    <?php } ?>
+                </div>
         </form>
     </div>
     <script type="text/javascript">
-    $('#lapolnb2bstartdate').datepicker({
-        dateFormat: "dd/mm/yy"
-    });
-    $('#lapolnb2benddate').datepicker({
-        dateFormat: "dd/mm/yy"
-    });
-    // $( "#lapolnb2bstartdate" ).datepicker( 'setDate', '<?php echo date('d/m/Y')?>' );
+        $('#lapolnb2bstartdate').datepicker({
+            dateFormat: "dd/mm/yy"
+        });
+        $('#lapolnb2benddate').datepicker({
+            dateFormat: "dd/mm/yy"
+        });
+        // $( "#lapolnb2bstartdate" ).datepicker( 'setDate', '<?php echo date('d/m/Y') ?>' );
     </script>
-    
+
 </div>
